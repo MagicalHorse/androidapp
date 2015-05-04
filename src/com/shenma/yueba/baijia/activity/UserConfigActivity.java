e692f308ea3a342c9a350123f327fe675d066ec2
zhangkaikai <646089668@qq.com>
2015-05-04 18:22:16 +0800

    jjjjj

---
+++ src/com/shenma/yueba/baijia/activity/UserConfigActivity.java
@@ -0,0 +1,16 @@
+package com.shenma.yueba.baijia.activity;
+
+import com.shenma.yueba.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UserConfigActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mefragmentforseller_config_layout);
+	}
+}
