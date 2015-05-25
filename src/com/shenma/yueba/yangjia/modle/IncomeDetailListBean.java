10cc0371244758faa0df4c4b842c9c72dc764cea
zhangkaikai <646089668@qq.com>
2015-05-25 11:48:37 +0800

    aaaa

---
+++ src/com/shenma/yueba/yangjia/modle/IncomeDetailListBean.java
@@ -0,0 +1,20 @@
+package com.shenma.yueba.yangjia.modle;
+
+public class IncomeDetailListBean {
+	private String date;
+	private String money;
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getMoney() {
+		return money;
+	}
+	public void setMoney(String money) {
+		this.money = money;
+	}
+	
+	
+}
