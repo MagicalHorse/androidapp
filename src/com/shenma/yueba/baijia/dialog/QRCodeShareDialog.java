cf802adf291befc3d25021100b37f42dafb24301
gyj2015 <gaoyj@joybar.com.cn>
2015-06-05 12:51:11 +0800

    11

--- src/com/shenma/yueba/baijia/dialog/QRCodeShareDialog.java
+++ src/com/shenma/yueba/baijia/dialog/QRCodeShareDialog.java
@@ -1,7 +1,6 @@
 package com.shenma.yueba.baijia.dialog;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
