a300884a81fb39ca9924c44ca48c0841c13b5c54
zhangkaikai <646089668@qq.com>
2015-06-05 10:14:27 +0800

    aaaa

--- src/com/shenma/yueba/yangjia/activity/AddCircleActivity.java
+++ src/com/shenma/yueba/yangjia/activity/AddCircleActivity.java
@@ -1,22 +1,54 @@
 package com.shenma.yueba.yangjia.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.shenma.yueba.R;
 import com.shenma.yueba.baijia.activity.BaseActivityWithTopView;
+import com.shenma.yueba.util.FontManager;
+import com.shenma.yueba.view.RoundImageView;
 
 /**
  * 新增圈子
  * @author a
  *
  */
-public class AddCircleActivity extends BaseActivityWithTopView{
-
+public class AddCircleActivity extends BaseActivityWithTopView implements OnClickListener{
+	
+	private TextView tv_cirlce_head_title;
+	private TextView tv_cirlce_name_title;
+	private EditText et_circle_name;
+	private RoundImageView riv_circle_head;
+	private RelativeLayout rl_head;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.add_circle_layout);
+		initView();
 		super.onCreate(savedInstanceState);
+	}
+	
+	
+	private void initView() {
+		tv_cirlce_head_title = getView(R.id.tv_cirlce_head_title);
+		tv_cirlce_name_title = getView(R.id.tv_cirlce_name_title);
+		et_circle_name = getView(R.id.et_circle_name);
+		riv_circle_head = getView(R.id.riv_circle_head);
+		rl_head = getView(R.id.rl_head);
+		rl_head.setOnClickListener(this);
+		FontManager.changeFonts(mContext, tv_cirlce_head_title,tv_cirlce_name_title,
+				et_circle_name,riv_circle_head,rl_head);
+	}
+
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
 	}
 }
---
+++ src/com/shenma/yueba/yangjia/activity/PublishProductForRenZhenActivity.java
@@ -0,0 +1,68 @@
+package com.shenma.yueba.yangjia.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.shenma.yueba.R;
+import com.shenma.yueba.baijia.activity.BaseActivityWithTopView;
+import com.shenma.yueba.util.FontManager;
+
+/**
+ * 发布商品（认证买手）
+ * @author a
+ *
+ */
+public class PublishProductForRenZhenActivity extends BaseActivityWithTopView implements OnClickListener{
+
+	private TextView tv_publish;//发布
+	private TextView tv_price_title;//
+	private EditText et_product_introduce;//
+	private TextView tv_retain;//还可以输入几个字
+	private TextView tv_add;//添加商品属性
+	private TextView tv_products_title;//
+	private LinearLayout ll_images_container;
+	private LinearLayout ll_guige_more;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.edit_product_layout);
+		initView();
+		super.onCreate(savedInstanceState);
+	}
+	private void initView() {
+		View.inflate(mContext, R.layout.guige_item_layuout, null);
+		tv_publish = getView(R.id.tv_publish);
+		tv_retain = getView(R.id.tv_retain);
+		ll_images_container = getView(R.id.ll_images_container);
+		tv_price_title = getView(R.id.tv_price_title);
+		et_product_introduce = getView(R.id.et_product_introduce);
+		tv_add = getView(R.id.tv_add);
+		tv_products_title = getView(R.id.tv_products_title);
+		ll_images_container = getView(R.id.ll_images_container);
+		ll_guige_more = getView(R.id.ll_guige_more);
+		tv_publish.setOnClickListener(this);
+		tv_add.setOnClickListener(this);
+		FontManager.changeFonts(mContext, tv_publish,tv_price_title,et_product_introduce,tv_add,
+				tv_products_title);
+	}
+	@Override
+	public void onClick(View v) {
+	
+		switch (v.getId()) {
+		case R.id.tv_publish://发布商品
+			
+			break;
+		case R.id.tv_add://添加商品属性
+			
+			break;
+		default:
+			break;
+		}
+		
+	}
+}
