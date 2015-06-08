a670cfe1727f652dead1d90a0a85c30d482bee41
zhangkaikai <646089668@qq.com>
2015-06-08 11:15:48 +0800

    aaa

--- src/com/shenma/yueba/baijia/activity/CircleInfoActivity.java
+++ src/com/shenma/yueba/baijia/activity/CircleInfoActivity.java
@@ -3,6 +3,7 @@ package com.shenma.yueba.baijia.activity;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,14 +14,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.shenma.yueba.R;
+import com.shenma.yueba.application.MyApplication;
 import com.shenma.yueba.baijia.adapter.MyCircleInfoAdapter;
 import com.shenma.yueba.baijia.dialog.QRCodeShareDialog;
-import com.shenma.yueba.baijia.modle.GridVIewItemBean;
 import com.shenma.yueba.util.FontManager;
+import com.shenma.yueba.util.HttpControl;
+import com.shenma.yueba.util.HttpControl.HttpCallBackInterface;
+import com.shenma.yueba.util.ToolsUtil;
 import com.shenma.yueba.view.MyGridView;
 import com.shenma.yueba.view.RoundImageView;
-import com.shenma.yueba.view.imageshow.ImageShowActivity;
 import com.shenma.yueba.yangjia.activity.ModifyCircleNameActivity;
+import com.shenma.yueba.yangjia.modle.CircleDetailBackBean;
+import com.shenma.yueba.yangjia.modle.CircleDetailBean;
+import com.shenma.yueba.yangjia.modle.Users;
 
 /**
  * 圈子信息
@@ -38,10 +44,11 @@ public class CircleInfoActivity extends BaseActivityWithTopView implements OnCli
 	private RoundImageView riv_circle_head;
 	private MyGridView gv_circle;
 	private MyCircleInfoAdapter adapter;
-	private List<GridVIewItemBean> mList = new ArrayList<GridVIewItemBean>();
+	private List<Users> mList = new ArrayList<Users>();
 	private RelativeLayout rl_head;
 	private RelativeLayout rl_circle_name;
 	private RelativeLayout rl_qrcode;
+	private String cricleId;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +56,13 @@ public class CircleInfoActivity extends BaseActivityWithTopView implements OnCli
 		setContentView(R.layout.circle_info_layout);
 		super.onCreate(savedInstanceState);
 		initView();
+		getIntentData();
+		getCircleDetail(cricleId, mContext, true);
+	}
+
+	private void getIntentData() {
+		cricleId = getIntent().getStringExtra("circleId");
+		
 	}
 
 	private void initView() {
@@ -71,20 +85,6 @@ public class CircleInfoActivity extends BaseActivityWithTopView implements OnCli
 		rl_circle_name.setOnClickListener(this);
 		rl_qrcode = getView(R.id.rl_qrcode);
 		rl_qrcode.setOnClickListener(this);
-		GridVIewItemBean bean = new GridVIewItemBean();
-		bean.setHead("http://img1.imgtn.bdimg.com/it/u=3176667768,1161431363&fm=21&gp=0.jpg");
-		bean.setName("张三");
-		GridVIewItemBean bean2 = new GridVIewItemBean();
-		bean2.setHead("http://img1.imgtn.bdimg.com/it/u=3176667768,1161431363&fm=21&gp=0.jpg");
-		bean2.setName("张三2");
-		GridVIewItemBean bean3 = new GridVIewItemBean();
-		bean3.setHead("http://img1.imgtn.bdimg.com/it/u=3176667768,1161431363&fm=21&gp=0.jpg");
-		bean3.setName("张3");
-		mList.add(bean2);
-		mList.add(bean);
-		mList.add(bean3);
-		mList.add(new GridVIewItemBean());
-		mList.add(new GridVIewItemBean());
 		adapter = new MyCircleInfoAdapter(mContext, mList);
 		gv_circle.setAdapter(adapter);
 		FontManager.changeFonts(mContext, tv_cirlce_head_title,tv_cirlce_name_title,
@@ -100,6 +100,8 @@ public class CircleInfoActivity extends BaseActivityWithTopView implements OnCli
 			break;
 		case R.id.rl_circle_name://更改姓名
 			Intent modifyCircleNameIntent = new Intent(this,ModifyCircleNameActivity.class);
+			modifyCircleNameIntent.putExtra("circleId", cricleId);
+			modifyCircleNameIntent.putExtra("name", tv_circle_name.getText().toString().trim());
 			startActivity(modifyCircleNameIntent);
 			break;
 		case R.id.rl_qrcode://二维码查看
@@ -119,5 +121,42 @@ public class CircleInfoActivity extends BaseActivityWithTopView implements OnCli
 		default:
 			break;
 		}
+	}
+	
+	
+	/**
+	 * 获取圈子详情
+	 * @param ctx
+	 * @param isRefresh
+	 * @param showDialog
+	 */
+	public void getCircleDetail(String circleId,Context ctx,boolean showDialog){
+		HttpControl httpControl = new HttpControl();
+		httpControl.getCircleDetail(circleId, showDialog, new HttpCallBackInterface() {
+			
+			@Override
+			public void http_Success(Object obj) {
+				CircleDetailBackBean result = (CircleDetailBackBean) obj;
+				CircleDetailBean bean = result.getData();
+				if(bean!=null){
+					tv_circle_name.setText(ToolsUtil.nullToString(bean.getGroupName()));
+					MyApplication.getInstance().getImageLoader().displayImage(ToolsUtil.getImage(ToolsUtil.nullToString(bean.getGroupPic()), 120, 0), riv_circle_head);
+					List<Users> users = bean.getUsers();
+					if(users!=null && users.size()>0){
+						mList.addAll(users);
+						mList.addAll(users);
+						mList.addAll(users);
+						mList.add(new Users());
+						mList.add(new Users());
+						adapter.notifyDataSetChanged();
+					}
+				}
+			}
+			
+			@Override
+			public void http_Fails(int error, String msg) {
+				Toast.makeText(mContext, msg, 1000).show();
+			}
+		}, CircleInfoActivity.this);
 	}
 }
