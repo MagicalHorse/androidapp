55bc5632bfdb3d87cedcb85511185dddc4b801b0
a <a@192.168.1.143>
2015-06-13 14:50:04 +0800

    AAAAA

---
+++ src/com/shenma/yueba/yangjia/modle/RewardDetailBackBean.java
@@ -0,0 +1,21 @@
+package com.shenma.yueba.yangjia.modle;
+
+import com.shenma.yueba.baijia.modle.BaseRequest;
+import com.shenma.yueba.util.RewardDetailItem;
+
+public class RewardDetailBackBean extends BaseRequest{
+
+	private RewardDetailItem data;
+
+	public RewardDetailItem getData() {
+		return data;
+	}
+
+	public void setData(RewardDetailItem data) {
+		this.data = data;
+	}
+
+	
+	
+	
+}
