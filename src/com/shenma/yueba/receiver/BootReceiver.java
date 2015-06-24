4f3f3e2393cef8ce7920adf79db87a4e6760212e
a <a@192.168.1.143>
2015-06-24 09:22:30 +0800

    aaa

--- src/com/shenma/yueba/receiver/BootReceiver.java
+++ src/com/shenma/yueba/receiver/BootReceiver.java
@@ -12,6 +12,7 @@ import android.content.Intent;
  */
 public class BootReceiver extends BroadcastReceiver  {
 
+	
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		 if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {//程序启动完成  
