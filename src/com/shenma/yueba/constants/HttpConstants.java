c73f168477ddefc3b3d4eac0689a1927c97a225c
Administrator <Administrator@192.168.1.177>
2015-07-07 19:23:28 +0800

    1

--- src/com/shenma/yueba/constants/HttpConstants.java
+++ src/com/shenma/yueba/constants/HttpConstants.java
@@ -647,22 +647,6 @@ public class HttpConstants {
 	 * **/
 	public static String METHOD_SEARCH = SEARCH+ "Search";
 	
-	
-	/*****
-	 *获取圈子房间号
-	 * **/
-	public static String METHOD_GETROOMID = Circle+ "GetRoom";
-	
-	
-	/*****
-	 *获取房间消息
-	 * **/
-	public static String METHOD_GETROOMMESSAE = Circle+ "GetMessages";
-	
-	
-	
-	
-	
 	/*****
 	 *微信支付回调url
 	 * **/
