41333574ce79f0ed2d436cce1303584be9aad250
a <a@192.168.1.143>
2015-07-13 11:26:16 +0800

    aaaaaaaa

--- src/com/shenma/yueba/view/TagImageView.java
+++ src/com/shenma/yueba/view/TagImageView.java
@@ -29,6 +29,7 @@ import com.shenma.yueba.application.MyApplication;
 public class TagImageView extends RelativeLayout {
 	
 	
+	
 	private ArrayList<String> tagNameList =new ArrayList<String>();//姓名的集合
 	private int i;
 	/** 上下文对象 */
