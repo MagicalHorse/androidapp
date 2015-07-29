3820b2d44caa6f73b1aec8e804b0350c1dea5e79
a <a@192.168.1.143>
2015-07-29 11:20:22 +0800

    aaaaa

--- src/com/shenma/yueba/receiver/ConnectionChangeReceiver.java
+++ src/com/shenma/yueba/receiver/ConnectionChangeReceiver.java
@@ -22,14 +22,14 @@ public class ConnectionChangeReceiver extends BroadcastReceiver {
 		NetworkInfo mobNetInfo = connectivityManager
 				.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
 		if (activeNetInfo != null) {
-			Toast.makeText(context,
-					"Active Network Type : " + activeNetInfo.getTypeName(),
-					Toast.LENGTH_SHORT).show();
+//			Toast.makeText(context,
+//					"Active Network Type : " + activeNetInfo.getTypeName(),
+//					Toast.LENGTH_SHORT).show();
 		}
 		if (mobNetInfo != null) {
-			Toast.makeText(context,
-					"Mobile Network Type : " + mobNetInfo.getTypeName(),
-					Toast.LENGTH_SHORT).show();
+//			Toast.makeText(context,
+//					"Mobile Network Type : " + mobNetInfo.getTypeName(),
+//					Toast.LENGTH_SHORT).show();
 		}
 	}
 }
