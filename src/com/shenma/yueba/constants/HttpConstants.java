1dbdce9f8e1bb40d00d7dff308da12854417dde5
gyj <gaoyj@joybar.com.cn>
2015-08-07 18:33:58 +0800

    1

--- src/com/shenma/yueba/constants/HttpConstants.java
+++ src/com/shenma/yueba/constants/HttpConstants.java
@@ -2,8 +2,8 @@ package com.shenma.yueba.constants;
 
 public class HttpConstants {
 	
-    //private static String baseUrl = "http://123.57.77.86:8080/app/";//测试地址
-    private static String baseUrl = "http://123.57.52.187:8080/app/";//开发地址
+    private static String baseUrl = "http://123.57.77.86:8080/app/";//测试地址
+//    private static String baseUrl = "http://123.57.52.187:8080/app/";//开发地址
 	private static String USERURL = baseUrl + "User/";// USER接口
 	private static String COMMONURL = baseUrl + "Common/";
 	private static String ADDRESS = baseUrl + "Address/";// 地址管理
