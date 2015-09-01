d720008adaa8290a80cbee841742a4dff5f35e90
a <a@192.168.1.143>
2015-09-01 11:50:13 +0800

    a

--- src/com/shenma/yueba/baijia/dialog/QRCodeShareDialog.java
+++ src/com/shenma/yueba/baijia/dialog/QRCodeShareDialog.java
@@ -1,110 +1,132 @@
 package com.shenma.yueba.baijia.dialog;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-
+import u.aly.bi;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.shenma.yueba.R;
 import com.shenma.yueba.application.MyApplication;
 import com.shenma.yueba.util.Base64Coder;
 import com.shenma.yueba.util.FontManager;
+import com.shenma.yueba.util.ShareUtil;
 import com.shenma.yueba.util.SharedUtil;
 import com.shenma.yueba.util.ToolsUtil;
+import com.shenma.yueba.util.ShareUtil.ShareListener;
 import com.shenma.yueba.view.RoundImageView;
 import com.shenma.yueba.yangjia.modle.kaixiaoPiaoBean;
 
-/**  
- * @author gyj  
- * @version 创建时间：2015-5-26 下午3:01:52  
- * 程序的简单说明   圈子二维码 分享对话框
+/**
+ * @author gyj
+ * @version 创建时间：2015-5-26 下午3:01:52 程序的简单说明 圈子二维码 分享对话框
  */
 
-public class QRCodeShareDialog extends Dialog implements android.view.View.OnClickListener{
+public class QRCodeShareDialog extends Dialog implements
+		android.view.View.OnClickListener {
 	Context context;
 	RelativeLayout ll;
 	ImageView qzcodeshare_layouyt_close_imageview;
 	TextView qzcodeshare_layouyt_name_textview;
+	TextView tv_share;
 	TextView qzcodeshare_layout_title_textview;
-	Button qzcodeshare_layout_share_button;
 	kaixiaoPiaoBean obj;
 	private TextView qzcodeshare_layouyt_count_textview;
 	private ImageView qzcodeshare_layout_content_imageview;
-	public QRCodeShareDialog(Context context,kaixiaoPiaoBean obj) {
+	private Bitmap bitmap;
+
+	public QRCodeShareDialog(Context context, kaixiaoPiaoBean obj) {
 		super(context, R.style.MyDialog);
 		requestWindowFeature(getWindow().FEATURE_NO_TITLE);
-		this.context=context;
-		this.obj=obj;
-		setOwnerActivity((Activity)context);
+		this.context = context;
+		this.obj = obj;
+		setOwnerActivity((Activity) context);
 		setCancelable(true);
 		setCanceledOnTouchOutside(false);
-        //this.getWindow().setBackgroundDrawable(context.getResources().getDrawable(R.color.color_transparent));
+		// this.getWindow().setBackgroundDrawable(context.getResources().getDrawable(R.color.color_transparent));
 	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
+
 		super.onCreate(savedInstanceState);
-		ll=(RelativeLayout)RelativeLayout.inflate(context, R.layout.qzcodeshare_layout, null);
+		ll = (RelativeLayout) RelativeLayout.inflate(context,
+				R.layout.qzcodeshare_layout, null);
 		setContentView(ll);
 		initView();
 	}
-	
-	void initView()
-	{
-		RoundImageView qzcodeshare_layouyt_customview = (RoundImageView) ll.findViewById(R.id.qzcodeshare_layouyt_customview);
-		initBitmap(SharedUtil.getHeadImage(getContext()), qzcodeshare_layouyt_customview);
-		qzcodeshare_layouyt_close_imageview=(ImageView)ll.findViewById(R.id.qzcodeshare_layouyt_close_imageview);
-		qzcodeshare_layout_content_imageview = (ImageView)ll.findViewById(R.id.qzcodeshare_layout_content_imageview);
-		qzcodeshare_layout_title_textview=(TextView)ll.findViewById(R.id.qzcodeshare_layout_title_textview);
-		qzcodeshare_layouyt_name_textview=(TextView)ll.findViewById(R.id.qzcodeshare_layouyt_name_textview);
-		qzcodeshare_layouyt_count_textview = (TextView)ll.findViewById(R.id.qzcodeshare_layouyt_count_textview);
-		qzcodeshare_layout_share_button=(Button)ll.findViewById(R.id.qzcodeshare_layout_share_button);
-		qzcodeshare_layout_share_button.setOnClickListener(this);
+
+	void initView() {
+		RoundImageView qzcodeshare_layouyt_customview = (RoundImageView) ll
+				.findViewById(R.id.qzcodeshare_layouyt_customview);
+		initBitmap(SharedUtil.getHeadImage(getContext()),
+				qzcodeshare_layouyt_customview);
+		qzcodeshare_layouyt_close_imageview = (ImageView) ll
+				.findViewById(R.id.qzcodeshare_layouyt_close_imageview);
+		qzcodeshare_layout_content_imageview = (ImageView) ll
+				.findViewById(R.id.qzcodeshare_layout_content_imageview);
+		qzcodeshare_layout_title_textview = (TextView) ll
+				.findViewById(R.id.qzcodeshare_layout_title_textview);
+		qzcodeshare_layouyt_name_textview = (TextView) ll
+				.findViewById(R.id.qzcodeshare_layouyt_name_textview);
+		qzcodeshare_layouyt_count_textview = (TextView) ll
+				.findViewById(R.id.qzcodeshare_layouyt_count_textview);
+		tv_share = (TextView) ll.findViewById(R.id.tv_share);
+		tv_share.setOnClickListener(this);
 		qzcodeshare_layouyt_close_imageview.setOnClickListener(this);
-		qzcodeshare_layouyt_name_textview.setText("商品名："+obj.getProductName());
-		qzcodeshare_layouyt_count_textview.setText("￥"+obj.getAmount());
+		qzcodeshare_layouyt_name_textview
+				.setText("商品名：" + obj.getProductName());
+		qzcodeshare_layouyt_count_textview.setText("￥" + obj.getAmount());
 		byte[] bytes = Base64Coder.decode(obj.getQrCode());
-		LayoutParams params = qzcodeshare_layout_content_imageview.getLayoutParams();
-		params.width = ToolsUtil.getDisplayWidth(context)/4*3;
-		params.height = ToolsUtil.getDisplayWidth(context)/4*3;
+		LayoutParams params = qzcodeshare_layout_content_imageview
+				.getLayoutParams();
+		params.width = ToolsUtil.getDisplayWidth(context) / 4 * 3;
+		params.height = ToolsUtil.getDisplayWidth(context) / 4 * 3;
 		qzcodeshare_layout_content_imageview.setLayoutParams(params);
-		qzcodeshare_layout_content_imageview.setImageBitmap(BitmapFactory.decodeByteArray(bytes, 0, bytes.length));
-		FontManager.changeFonts(context,qzcodeshare_layouyt_name_textview,qzcodeshare_layouyt_count_textview);
+		bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+		qzcodeshare_layout_content_imageview.setImageBitmap(bitmap);
+		FontManager.changeFonts(context, qzcodeshare_layouyt_name_textview,
+				qzcodeshare_layouyt_count_textview,tv_share);
 	}
-	
+
 	@Override
 	public void show() {
-		
 		super.show();
-		
+
 	}
 
 	@Override
 	public void onClick(View v) {
-		
-		switch(v.getId())
-		{
+		switch (v.getId()) {
 		case R.id.qzcodeshare_layouyt_close_imageview:
 			this.cancel();
 			break;
-		case R.id.qzcodeshare_layout_share_button:
+		case R.id.tv_share:// 分享给好友
+			this.cancel();
+			ShareUtil.shareAll2((Activity)context,"扫码商品分享", "亲，帮我代付下呗", "", bitmap, new ShareListener() {
+				@Override
+				public void sharedListener_sucess() {
+					Toast.makeText(context, "分享成功", 1000).show();
+				}
+
+				@Override
+				public void sharedListener_Fails(String msg) {
+					Toast.makeText(context, "分享失败", 1000).show();
+				}
+			});
 			break;
 		}
 	}
-	
-	void initBitmap(final String url, final ImageView iv)
-	{
+
+	void initBitmap(final String url, final ImageView iv) {
 		MyApplication.getInstance().getBitmapUtil().display(iv, url);
 	}
 }
--- src/com/shenma/yueba/util/ShareUtil.java
+++ src/com/shenma/yueba/util/ShareUtil.java
@@ -8,6 +8,7 @@ import java.util.List;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.text.TextUtils;
 import android.view.Gravity;
@@ -57,6 +58,27 @@ public class ShareUtil {
 		getShareDialog(activity,title,content,url,imgurl,shareListener);
 	}
 
+	public static void shareAll2(final Activity activity, final String title,final String content,final String url, final Bitmap bitmap,ShareListener shareListener) {
+		List<String> list = new ArrayList<String>();
+//		if (SharedUtil.getWeiboQQ(activity)) {
+//			list.add("腾讯微博");
+//		}
+//		if (SharedUtil.getWeiboSina(activity)) {
+//			list.add("新浪微博");
+//		}
+		if (SharedUtil.getWeixinFriends(activity)) {
+			list.add("微信好友");
+		}
+		if (SharedUtil.getFriendCircle(activity)) {
+			list.add("微信朋友圈");
+		}
+		final String[] strArray = new String[list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			strArray[i] = list.get(i);
+		}
+		getShareDialog(activity,title,content,url,bitmap,shareListener);
+	}
+	
 	// 微信好友
 	public static void shareWeixinFriend(final Activity activity,String title,String content, String url, String imgurl,final ShareListener shareListener) {
 		// 添加微信平台
@@ -159,6 +181,100 @@ public class ShareUtil {
 
 	
 	
+	// 微信好友
+		public static void shareWeixinFriend(final Activity activity,String title,String content, String url, Bitmap bitmap,final ShareListener shareListener) {
+			// 添加微信平台
+			UMWXHandler wxHandler = new UMWXHandler(activity, appId, appSecret);
+			wxHandler.addToSocialSDK();
+			WeiXinShareContent weixinContent = new WeiXinShareContent();
+			weixinContent.setTitle(title);
+			weixinContent.setShareContent(content);
+			weixinContent.setTargetUrl(url);
+				weixinContent.setShareImage(new UMImage(activity, bitmap));
+			UMSocialService controller = UMServiceFactory.getUMSocialService(
+					"com.umeng.share", RequestType.SOCIAL);
+			controller.setShareMedia(weixinContent);
+			controller.postShare(activity, SHARE_MEDIA.WEIXIN,
+					new SnsPostListener() {
+
+						@Override
+						public void onStart() {
+						}
+
+						@Override
+						public void onComplete(SHARE_MEDIA arg0, int arg1,
+								SocializeEntity arg2) {
+							if (200 == arg1) {
+								if(shareListener!=null)
+								{
+									shareListener.sharedListener_sucess();
+								}
+								//Toast.makeText(activity, "分享成功", Toast.LENGTH_SHORT).show();
+							} else {
+								if(shareListener!=null)
+								{
+									shareListener.sharedListener_Fails("分享失败");
+								}
+								//Toast.makeText(activity, "分享失败", Toast.LENGTH_SHORT).show();
+							}
+						}
+					});
+		}
+
+		// 微信朋友圈
+		public static void shareWeixinFriends(final Activity activity,String title,
+				String content, String url, Bitmap bitmap,final ShareListener shareListener) {
+			UMWXHandler wxCircleHandler = new UMWXHandler(activity, appId, appSecret);
+			wxCircleHandler.setToCircle(true);
+			wxCircleHandler.addToSocialSDK();
+			CircleShareContent circleMedia = new CircleShareContent();
+		
+			if(!TextUtils.isEmpty(content)){
+				circleMedia.setTitle(content);
+			}
+			
+			if(!TextUtils.isEmpty(content)){
+				circleMedia.setShareContent(content);
+			}
+				circleMedia.setShareImage(new UMImage(activity, bitmap));
+			circleMedia.setTargetUrl(url);
+			com.umeng.socialize.utils.Log.LOG = true;
+			UMSocialService controller = UMServiceFactory.getUMSocialService("com.umeng.share", RequestType.SOCIAL);
+			controller.setShareMedia(circleMedia);
+			controller.getConfig().closeToast();
+			
+			controller.postShare(activity, SHARE_MEDIA.WEIXIN_CIRCLE,
+					new SnsPostListener() {
+
+						@Override
+						public void onComplete(SHARE_MEDIA arg0, int arg1,
+								SocializeEntity arg2) {
+							if (200 == arg1) {
+								if(shareListener!=null)
+								{
+									shareListener.sharedListener_sucess();
+								}
+								//Toast.makeText(activity, "分享成功", Toast.LENGTH_SHORT).show();
+							} else {
+								if(shareListener!=null)
+								{
+									shareListener.sharedListener_Fails("分享失败");
+								}
+								//Toast.makeText(activity, "分享失败", Toast.LENGTH_SHORT).show();
+							}
+						}
+
+						
+						@Override
+						public void onStart() {
+							
+						}
+
+					});
+		}
+
+		
+	
 	
 	
 	
@@ -260,6 +376,60 @@ public class ShareUtil {
 						shareWeixinFriend((Activity)ctx,title, content, url, imgurl,shareListener);
 					}else if("微信朋友圈".equals(list.get(position))){
 						shareWeixinFriends((Activity)ctx, title,content, url, imgurl,shareListener);
+					}
+				}else{
+					Toast.makeText(ctx, "网络不可用，请检查网络连接", 1000).show();
+				}
+			
+			}
+		});
+		bt_quit.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				pop.dismiss();
+			}
+		});
+	}
+	
+	
+	
+	public static void getShareDialog(final Context ctx,final String title,final String content,
+			final String url, final Bitmap bitmap,final ShareListener shareListener){
+		final List<String> list = new ArrayList<String>();
+//		if (SharedUtil.getWeiboQQ(ctx)) {
+//			list.add("腾讯微博");
+//		}
+//		if (SharedUtil.getWeiboSina(ctx)) {
+//			list.add("新浪微博");
+//		}
+		if (SharedUtil.getWeixinFriends(ctx)) {
+			list.add("微信好友");
+		}
+		if (SharedUtil.getFriendCircle(ctx)) {
+			list.add("微信朋友圈");
+		}
+		
+		
+		View view = View.inflate(ctx, R.layout.share_layout2, null);
+		MyGridView mgv = (MyGridView) view.findViewById(R.id.mgv);
+		Button bt_quit = (Button) view.findViewById(R.id.bt_quit);
+		pop = new BottomPopupWindow((Activity)ctx, view);
+		pop.showAtLocation(view.findViewById(R.id.parent), Gravity.BOTTOM| Gravity.CENTER_HORIZONTAL, 0, 0);
+		mgv.setAdapter(new ShareAdapter(ctx, list));
+		mgv.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+					long arg3) {
+				if(ToolsUtil.isNetworkConnected(ctx)){
+					pop.dismiss();
+					if("腾讯微博".equals(list.get(position))){
+						//share((Activity)ctx, SHARE_MEDIA.TENCENT, content+"-->请点击："+url,imgurl);
+					}else if("新浪微博".equals(list.get(position))){
+						//share((Activity)ctx, SHARE_MEDIA.SINA, content+"-->请点击："+url,imgurl);
+					}else if("微信好友".equals(list.get(position))){
+						shareWeixinFriend((Activity)ctx,title, content, url, bitmap,shareListener);
+					}else if("微信朋友圈".equals(list.get(position))){
+						shareWeixinFriends((Activity)ctx, title,content, url, bitmap,shareListener);
 					}
 				}else{
 					Toast.makeText(ctx, "网络不可用，请检查网络连接", 1000).show();
